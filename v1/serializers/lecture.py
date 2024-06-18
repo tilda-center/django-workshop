bb8051a5dcffb85277cb87da91716ddf8d8d5de2
bot_expert <darkodare97@gmail.com>
2024-06-18 19:03:07 +0200

    Fixed Lecture endpoints

--- v1/serializers/lecture.py
+++ v1/serializers/lecture.py
@@ -3,15 +3,20 @@ from v1.models import Lecture
 
 
 class LectureSerializer(serializers.ModelSerializer):
+    professor = serializers.SerializerMethodField()
+
     class Meta:
         model = Lecture
-        read_only_fields = ["professor"]
         fields = [
             "title",
             "info",
+            "professor",
         ]
+
+    def get_professor(self, obj):
+        return obj.professor.first_name + " " + obj.professor.last_name
 
     def create(self, validated_data):
         validated_data["professor"] = self.context["request"].user
-        return super.create(validated_data)
+        return super().create(validated_data)
 
